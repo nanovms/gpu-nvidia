nanovms/gpu-nvidia

commit b4b0d76910189204f25a7b98484cf16ad4d7f089

strings: use sstring type when interfacing to main kernel
Change made necessary by
https://github.com/nanovms/nanos/commit/364b517d03a7296e8a5cb0326292d87efac44e07
Handling of null-terminated strings in this klib is now implemented
in kernel-open/nvidia/os-interface.c

kernel-open/nvidia/nv-frontend.c
@@ -142,7 +142,7 @@ int nvidia_register_module(nvidia_module_t *module)
         rc = -ENOMEM;
         goto done;
     }
-    if (!create_special_file("/dev/nvidiactl", open, 0,
+    if (!create_special_file(ss("/dev/nvidiactl"), open, 0,
         makedev(NV_MAJOR_DEVICE_NUMBER, NV_CONTROL_DEVICE_MINOR))) {
         deallocate_closure(open);
         rc = -ENOSPC;
@@ -219,7 +219,7 @@ int nvidia_frontend_add_device(nvidia_module_t *module, nv_nanos_state_t * devic
                     file_path[12] = '0' + (minor / 10) % 10;
                     file_path[13] = '0' + minor % 10;
                 }
-                if (!create_special_file(file_path, open, 0,
+                if (!create_special_file(isstring(file_path, sizeof(file_path) - 1), open, 0,
                     makedev(NV_MAJOR_DEVICE_NUMBER, minor))) {
                     deallocate_closure(open);
                     rc = -1;

kernel-open/common/inc/nv-nanos.h
@@ -403,12 +403,14 @@ typedef struct nvidia_event
 #define memcpy(d, s, n)         ({ runtime_memcpy(d, s, n); d; })
 #define memset(p, v, l)         ({ runtime_memset((void *)(p), v, l); (void *)(p); })
 #define memcmp                  runtime_memcmp
-#define strcmp                  runtime_strcmp
-#define strlen                  runtime_strlen
-#define strstr                  runtime_strstr
-#define strchr                  runtime_strchr
+#define strcmp                  os_string_compare
+#define strlen                  os_string_length
+#define strstr(_h, _n)          \
+    runtime_strstr(isstring((char *)(_h), os_string_length(_h)),    \
+                   isstring((char *)(_n), os_string_length(_n)))
+#define strchr(_s, _c)          runtime_strchr(isstring((char *)(_s), os_string_length(_s)), _c)
 #define strncpy(dest, src, len) ({  \
-    runtime_memcpy(dest, src, MIN(runtime_strlen(src) + 1, len)); dest; \
+    runtime_memcpy(dest, src, MIN(os_string_length(src) + 1, len)); dest; \
 })
 #define snprintf                rsnprintf
 

kernel-open/common/inc/nv-proto.h
@@ -27,7 +27,7 @@
 #include "nv-pci.h"
 #include "nv-register-module.h"
 
-extern const char *nv_device_name;
+extern const sstring nv_device_name;
 extern nvidia_module_t nv_fops;
 
 void        nv_acpi_register_notifier   (nv_nanos_state_t *);

kernel-open/nvidia-uvm/uvm.c
@@ -421,7 +421,7 @@ static int uvm_chardev_create(void)
 
     open = closure(heap_locked(get_kernel_heaps()), uvm_open);
     assert(open != INVALID_ADDRESS);
-    if (create_special_file("/dev/nvidia-uvm", open, sizeof(struct uvm_fd), 0)) {
+    if (create_special_file(ss("/dev/nvidia-uvm"), open, sizeof(struct uvm_fd), 0)) {
         return 0;
     } else {
         UVM_ERR_PRINT("create_special_file failed\n");

kernel-open/nvidia-uvm/uvm_tools.c
@@ -1957,7 +1957,7 @@ int uvm_tools_init(dev_t uvm_base_dev)
 
     open = closure(heap_locked(get_kernel_heaps()), uvm_tools_open);
     assert(open != INVALID_ADDRESS);
-    if (!create_special_file("/dev/nvidia-uvm-tools", open, 0, MAJOR(uvm_tools_dev))) {
+    if (!create_special_file(ss("/dev/nvidia-uvm-tools"), open, 0, MAJOR(uvm_tools_dev))) {
         UVM_ERR_PRINT("cdev_add (major %u, minor %u) failed: %d\n", MAJOR(uvm_tools_dev),
                       MINOR(uvm_tools_dev), ret);
         goto err_stop_thread;

kernel-open/nvidia/linux_nvswitch.c
@@ -306,11 +306,12 @@ nvswitch_os_print
 )
 {
     buffer b = little_stack_buffer(16 * KB);
-    buffer f = alloca_wrap_cstring(fmt);
     va_list arglist;
+    NvS32 char_count;
 
     va_start(arglist, fmt);
-    vbprintf(b, f, &arglist);
+    char_count = os_vsnprintf(buffer_ref(b, 0), b->length, fmt, arglist);
+    buffer_produce(b, MIN(char_count, b->length - 1));
     buffer_print(b);
     va_end(arglist);
 }
@@ -764,8 +765,8 @@ nvswitch_os_strncmp
     NvLength length
 )
 {
-    NvLength l1 = runtime_strlen(s1) + 1;
-    NvLength l2 = runtime_strlen(s2) + 1;
+    NvLength l1 = os_string_length(s1) + 1;
+    NvLength l2 = os_string_length(s2) + 1;
     length = MIN(length, l1);
     length = MIN(length, l2);
     return memcmp(s1, s2, length);
@@ -779,7 +780,7 @@ nvswitch_os_strncat
     NvLength length
 )
 {
-    NvLength l1 = runtime_strlen(s1);
+    NvLength l1 = os_string_length(s1);
     s1[l1 + length] = '\0';
     return strncpy (s1 + l1, s2, length);
 }
@@ -895,11 +896,12 @@ nvswitch_os_assert_log
 {
     va_list arglist;
     char fmt_printk[NVSWITCH_LOG_BUFFER_SIZE];
+    NvS32 char_count;
 
     va_start(arglist, fmt);
-    os_vsnprintf(fmt_printk, sizeof(fmt_printk), fmt, arglist);
+    char_count = os_vsnprintf(fmt_printk, sizeof(fmt_printk), fmt, arglist);
     va_end(arglist);
-    buffer_print(alloca_wrap_cstring(fmt_printk));
+    rput_sstring(isstring(fmt_printk, MIN(char_count, sizeof(fmt_printk) - 1)));
      dbg_breakpoint();
 }
 

kernel-open/nvidia/nv.c
@@ -164,7 +164,7 @@ static int          nvos_free_alloc(nv_alloc_t *);
 static int           nvidia_ctl_open        (nvfd nvlfp);
 static int           nvidia_ctl_close       (nvfd nvlfp);
 
-const char *nv_device_name = MODULE_NAME;
+const sstring nv_device_name = ss_static_init(MODULE_NAME);
 
 static sysreturn nvfd_open(u32 minor, file f);
 
@@ -653,7 +653,7 @@ static void *nv_alloc_file_private(void)
     if (!nvlfp)
         return NULL;
 
-    nvlfp->waitqueue = allocate_blockq(heap_locked(get_kernel_heaps()), "nvidia gpu");
+    nvlfp->waitqueue = allocate_blockq(heap_locked(get_kernel_heaps()), ss("nvidia gpu"));
     if (nvlfp->waitqueue == INVALID_ADDRESS)
     {
         NV_KFREE(nvlfp, sizeof(*nvlfp));
@@ -3072,7 +3072,7 @@ void NV_API_CALL nv_fetch_firmware(
     pagecache_node pn;
     range r;
 
-    fsf = fsfile_open(alloca_wrap_cstring(file_path));
+    fsf = fsfile_open(isstring((char *)file_path, os_string_length(file_path)));
     if (!fsf)
         return;
     len = fsfile_get_length(fsf);
@@ -3121,7 +3121,7 @@ const void* NV_API_CALL nv_get_firmware(
     buffer_handler bh;
     status_handler sh;
 
-    fwfile_v = split(h, alloca_wrap_cstring(file_path), '/');
+    fwfile_v = split(h, alloca_wrap_buffer(file_path, os_string_length(file_path)), '/');
     fwfile_t = resolve_path(filesystem_getroot(fs), fwfile_v);
     split_dealloc(fwfile_v);
     if (!fwfile_t)

kernel-open/nvidia/nvlink_linux.c
@@ -218,8 +218,7 @@ void nvlink_free(void *ptr)
 
 char * nvlink_strcpy(char *dest, const char *src)
 {
-    runtime_memcpy(dest, src, runtime_strlen(src) + 1);
-    return dest;
+    return os_string_copy(dest, src);
 }
 
 int nvlink_strcmp(const char *dest, const char *src)

commit dc8dbcdc88a6e8ab96fe86ad351e1730cc16588c

nvidia_frontend_add_device(): fix special file path length
The length of the special file path "/dev/nvidiaXXX" depends on the
minor number of the special file.
This fixes a "NVRM nvAssertFailedNoLog: Assertion failed:
rmapiLockIsOwner() && rmGpuLockIsOwner() @ conf_compute_api.c:76"
error that occurs during initialization of a CUDA application (bug
introduced in commit b4b0d769).

kernel-open/nvidia/nv-frontend.c
@@ -206,20 +206,22 @@ int nvidia_frontend_add_device(nvidia_module_t *module, nv_nanos_state_t * devic
 
             if (open != INVALID_ADDRESS) {
                 char file_path[] = "/dev/nvidiaXXX";
+                bytes file_path_len;
 
                 if (minor < 10) {
                     file_path[11] = '0' + minor;
-                    file_path[12] = '\0';
+                    file_path_len = 12;
                 } else if (minor < 100) {
                     file_path[11] = '0' + minor / 10;
                     file_path[12] = '0' + minor % 10;
-                    file_path[13] = '\0';
+                    file_path_len = 13;
                 } else {
                     file_path[11] = '0' + minor / 100;
                     file_path[12] = '0' + (minor / 10) % 10;
                     file_path[13] = '0' + minor % 10;
+                    file_path_len = 14;
                 }
-                if (!create_special_file(isstring(file_path, sizeof(file_path) - 1), open, 0,
+                if (!create_special_file(isstring(file_path, file_path_len), open, 0,
                     makedev(NV_MAJOR_DEVICE_NUMBER, minor))) {
                     deallocate_closure(open);
                     rc = -1;